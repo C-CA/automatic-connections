b4523a51da6d654a1b13772b946f9723b9286247
tfahry <75729710+tfahry@users.noreply.github.com>
2021-02-11 17:36:34 +0000

    Initial commit with ScotRail reader.

---
+++ UnitDiagramReader.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 15:33:07 2021
+
+@author: Tfahry
+
+Unit diagram reader.
+"""
+from pandas import read_excel
+
+class Reader:
+    def __init__(self,pathToUD):
+        self.ud = self.Parse(pathToUD)
+    
+    def Parse(self, pathToUD):
+        raise NotImplementedError('ParseUD not implemented')
+
+
+class ScotRailReader(Reader):
+    def Parse(self, pathToUD):
+        return read_excel(pathToUD, usecols=[0,1,2,4], header=6, dtype = str).fillna('UDNONE')
