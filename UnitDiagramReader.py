b0407f60b1cb8871379e97b8e898200f3c533ecb
tfahry <75729710+tfahry@users.noreply.github.com>
2021-02-15 10:41:18 +0000

    Unit Diagram Reader now has EmptyFill defined in parent class instead of being a string literal

--- UnitDiagramReader.py
+++ UnitDiagramReader.py
@@ -10,12 +10,13 @@ from pandas import read_excel
 
 class Reader:
     def __init__(self,pathToUD):
+        self.EmptyFill = 'UDNONE'
         self.ud = self.Parse(pathToUD)
     
     def Parse(self, pathToUD):
-        raise NotImplementedError('ParseUD not implemented')
+        raise NotImplementedError('Parse() not implemented')
 
 
 class ScotRailReader(Reader):
     def Parse(self, pathToUD):
-        return read_excel(pathToUD, usecols=[0,1,2,4], header=6, dtype = str).fillna('UDNONE')
+        return read_excel(pathToUD, usecols=[0,1,2,4], header=6, dtype = str).fillna(self.EmptyFill)
